5f0150533922d8b986b87309d01593ba54b00f82
FellpsGN <fellps.program06@gmail.com>
2025-03-24 18:32:36 -0300

    Add FormControl, FormGroup, ReactiveFormsModule
    Add import to do a "apply" form in page details, added inputs in template and labels using attribute [formGroup] and formControlName

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../../services/housing-service/housing.service';
 import { HousingLocation } from '../../models/housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -14,6 +15,11 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
